c8ada9b3bd9e7aed0b44998389def786a9bc75fc
aoshisen <aoshisen.23@gmail.com>
2025-12-02 17:04:56 +0800

    feat: 完成所有的联系

--- exercises/08_futures/04_future/src/lib.rs
+++ exercises/08_futures/04_future/src/lib.rs
@@ -2,15 +2,15 @@
 //!  in the `example` function. You're not allowed to change the
 //!  `spawner` function nor what each line does in `example`.
 //!   You can wrap existing statements in blocks `{}` if needed.
-use std::rc::Rc;
+use std::{rc::Rc, sync::Arc};
 use tokio::task::yield_now;
+
+async fn example() {
+    let non_send = Arc::new(1);
+    yield_now().await;
+    println!("{non_send}");
+}
 
 fn spawner() {
     tokio::spawn(example());
-}
-
-async fn example() {
-    let non_send = Rc::new(1);
-    yield_now().await;
-    println!("{}", non_send);
 }
